bb00268faa68b99b157fa9b5988582c13a2fe18a
Jonathan Bartlett <j.w.bartlett@bath.ac.uk>
2022-12-16 17:26:10 +0000

    Beginning documentation of functions

--- R/gFormulaMI.R
+++ R/gFormulaMI.R
@@ -1,9 +1,46 @@
-#timePoints - number of follow-up timepoints (including baseline)
-#trtRegimes - list of treatment regimes of interest
-
-
+#' G-formula multiple imputation
+#'
+#' `gFormulaImpute` creates multiple imputed synthetic datasets of longitudinal histories under
+#' specified treatment regimes of interest, based on the G-formula.
+#'
+#' `gFormulaImpute` creates multiple imputed synthetic datasets of longitudinal histories under
+#' specified treatment regimes of interest, based on the G-formula. Specifically, to
+#' the observed data frame, an additional `nSim` rows are added in which all variables are set to
+#' missing, except the time-varying treatment variables. The latter are set to the values
+#' as specified in the `trtRegimes` argument. If multiple treatment regimes are specified,
+#' `nSim` rows are added for each of the treatment regimes.
+#'
+#' `gFormulaImpute` uses the `mice` package to impute the potential outcome values of the
+#' time-varying confounders and outcome in the synthetic datasets. Imputation is performed
+#' sequentially from left to right in the data frame. As such, the variables must be ordered
+#' in time in the input data frame, with the time-varying confounders at each time followed
+#' by the corresponding treatment variable at that time.
+#'
+#' Missing data
+#'
+#' `gFormulaImpute` returns an object of class `mids`. This can be analysed using the same
+#' methods that imputed datasets from `mice` can be analysed with. However, Rubin's standard
+#' pooling rules are not valid for analysis of the synthetic datasets. Instead, the synthetic
+#' variance estimator of Raghunathan et al (2003) must be used, as implemented in the
+#' `gFormulaAnalyse` function.
+#'
+#' @param data The observed data frame
+#' @param M The number of imputed datasets to generate
+#' @param trtVarStem String indicating the stem of the treatment variables
+#' @param timePoints Integer specifying the number of timepoints (including baseline)
+#' @param trtRegimes A vector specifying the treatment regime of interest, or a list of
+#' vectors specifying the treatment regimes of interest
+#' @param nSim The number of individuals to simulate in each imputed dataset. Defaults to
+#' number of individuals in observed data
+#' @param micePrintFlag TRUE/FALSE specifying whether the output from the call to mice
+#' should be printed
+#'
+#' @return an S3 object of class mids (multiply imputed dataset)
+#' @export
+#'
+#' @examples
 gFormulaImpute <- function(data, M=50, trtVarStem, timePoints, trtRegimes,
-                           micePrintFlag=FALSE, nSim=NULL) {
+                           nSim=NULL, micePrintFlag=FALSE) {
 
   if (class(data)=="mids") {
     missingData <- TRUE
@@ -96,6 +133,14 @@ gFormulaImpute <- function(data, M=50, trtVarStem, timePoints, trtRegimes,
   imps <- suppressWarnings(mice::as.mids(imps))
 }
 
+#' Analyse a set of gFormula synthetic imputed datasets
+#'
+#' @param fits Collection of model fits produced by a call of the form with(imps, lm(y~regime))
+#'
+#' @return
+#' @export
+#'
+#' @examples
 gFormulaAnalyse <- function(fits) {
 
   M <- length(fits$analyses)
--- demo.r
+++ demo.r
@@ -1,4 +1,4 @@
-n <- 500
+n <- 50000
 
 expit <- function(x) exp(x)/(1+exp(x))
 
@@ -15,14 +15,14 @@ obsData <- data.frame(l0=l0,a0=a0,l1=l1,a1=a1,l2=l2,a2=a2,y=y)
 
 rm(l0,a0,l1,a1,l2,a2,y)
 
-imps <- gFormulaImpute(obsData,M=50,trtVarStem="a",timePoints=3,trtRegimes=c(0,0,0))
+imps <- gFormulaImpute(obsData,M=50,trtVarStem="a",timePoints=3,trtRegimes=c(1,1,1))
 
 fit <- with(imps, lm(y~1))
 pool(fit)
 gFormulaAnalyse(fit)
 
 imps <- gFormulaImpute(obsData,M=50,trtVarStem="a",timePoints=3,
-                       trtRegimes=list(c(0,0,0), c(1,1,1)))
+                       trtRegimes=list(c(0,0,0), c(1,1,1)),nSim=100)
 fit <- with(imps, lm(y~regime))
 pool(fit)
 gFormulaAnalyse(fit)
